7809045365c08b869cf7ea5b3a8b2704f230cb29
theHATEM <hatehassel@gmail.com>
2026-04-25 11:34:59 +0300

    Add SwaggerGen UI with JWT bearer support

--- DLMS.API/Program.cs
+++ DLMS.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
@@ -16,7 +17,37 @@ try
               .Enrich.WithThreadId());
 
     builder.Services.AddControllers();
-    builder.Services.AddOpenApi();
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "DLMS API",
+            Version = "v1"
+        });
+
+        var securityScheme = new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Description = "Enter: Bearer {your JWT token}",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        };
+
+        options.AddSecurityDefinition("Bearer", securityScheme);
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            { securityScheme, Array.Empty<string>() }
+        });
+    });
 
     var app = builder.Build();
 
@@ -28,7 +59,12 @@ try
 
     if (app.Environment.IsDevelopment())
     {
-        app.MapOpenApi();
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "DLMS API v1");
+            options.DisplayRequestDuration();
+        });
     }
 
     app.UseHttpsRedirection();
